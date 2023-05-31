1d14dc0a0dcccd9c590253aeae991873aa04b5a1
Geannis1 <103528295+Geannis1@users.noreply.github.com>
2023-05-31 20:38:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-var color = "red";
-var btn = document.getElementById("button");
-setInterval(ChangeColor, 10000);
-btn.addEventListener("click", ChangeColor);
+let color = "red";
+let btn = document.getElementById("btn");
+setInterval(ChangeColor, 3000);
+
 function ChangeColor() {
-        if(color == "red"){
-            color = "yellow";
-            btn.style.backgroundColor = 'yellow';
-        }else if(culoare == "yellow"){
-            color = "green";
-            btn.style.backgroundColor = "green";
-        }else if(color == "green"){
-            color = "red";
-            btn.style.backgroundColor = "red";
+        switch(color){
+            case 'red':
+                color = "yellow";
+                btn.style.backgroundColor = "yellow";
+                break;
+            case 'yellow':
+                color = "green";                    
+                btn.style.backgroundColor = "green";
+                break;
+            case 'green':
+                color = "red";                    
+                btn.style.backgroundColor = "red";
         }
 };
-        
-        
