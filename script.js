da56cae4489d93fa0f1caf96b568529ad65096f7
Geannis1 <103528295+Geannis1@users.noreply.github.com>
2023-06-01 10:38:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-let color = "red";
+let color = ["red", "yellow", "green"];
+let i = 0;
 let btn = document.getElementById("btn");
-setInterval(changeColor, 3000);
+setInterval(changeColor, 10000);
 
 function changeColor() {
-        switch(color){
-            case 'red':
-                color = "yellow";
-                btn.style.backgroundColor = "yellow";
-                break;
-            case 'yellow':
-                color = "green";                    
-                btn.style.backgroundColor = "green";
-                break;
-            case 'green':
-                color = "red";                    
-                btn.style.backgroundColor = "red";
-        }
-};
+btn.style.backgroundColor = color[i];
+    ++i;
+    if(i == 3) {
+        i = 0;
+    }
+}
